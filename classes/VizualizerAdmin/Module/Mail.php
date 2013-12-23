8e543f754844b09794ceeee1fe5c5324b4e4b352
Naohisa Minagawa <info@clay-system.jp>
2013-12-23 16:01:03 +0900

    メール送信の処理を追加

---
+++ classes/VizualizerAdmin/Module/Mail.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright (C) 2012 Vizualizer All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Naohisa Minagawa <info@vizualizer.jp>
+ * @copyright Copyright (c) 2010, Vizualizer
+ * @license http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ * @since PHP 5.3
+ * @version   1.0.0
+ */
+
+/**
+ * オペレータにメールを送信する。
+ *
+ * @package VizualizerAdmin
+ * @author Naohisa Minagawa <info@vizualizer.jp>
+ */
+class VizualizerAdmin_Module_Mail extends Vizualizer_Plugin_Module
+{
+
+    function execute($params)
+    {
+        if($params->check("title") && $params->check("template")){
+            $post = Vizualizer::request();
+            $operatorId = $post["operator_id"];
+            $title = $params->get("title");
+            $templateName = $params->get("template");
+
+            $loader = new Vizualizer_Plugin("Admin");
+            $operator = $loader->loadModel("CompanyOperator");
+            $operator->findByPrimaryKey($operatorId);
+
+            $attr = Vizualizer::attr();
+            $template = $attr["template"];
+            $body = $template->fetch($templateName.".txt");
+
+            $mail = new Vizualizer_Sendmail();
+            $mail->setFrom($params->get("from"));
+            $mail->setTo($operator->email);
+            $mail->setSubject($title);
+            $mail->addBody($body);
+            $mail->send();
+        }
+    }
+}
